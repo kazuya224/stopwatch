ddcedc5ea9ceef0abf023236c471615d175ce68e
Kazuya Ogawa <jiashouyexiaochuan@gmail.com>
2026-02-14 08:14:18 +0900

    CORS設定

--- back/src/main/java/com/example/demo/config/WebConfig.java
+++ back/src/main/java/com/example/demo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 全てのパスを対象にする
-                .allowedOrigins("http://localhost:5173") // フロントエンドのURLを許可
+                .allowedOrigins("https://stopwatch-front.onrender.com", "http://localhost:5173") // フロントエンドのURLを許可
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 許可するメソッド
                 .allowedHeaders("*") // 全てのヘッダーを許可
                 .allowCredentials(true); // クッキーなどの送受信を許可する場合
